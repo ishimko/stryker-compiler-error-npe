fce9ee1d2aaf1a688e39bad5039714ee554b47e3
Ivan Shimko <vana.shimko@gmail.com>
2020-10-29 13:11:33 +0300

    More code to get some mutation score

--- UnderTest.Tests/Tests.cs
+++ UnderTest.Tests/Tests.cs
@@ -6,12 +6,24 @@ namespace UnderTest.Tests
     public class Tests
     {
         [Test]
-        public void Test()
+        public void Diff_Test()
         {
             DateTime now = DateTime.Now;
             TimeSpan diff = TimeSpan.FromDays(5);
             TimeSpan actual = new DateTimeCalculator().Diff(now, now - diff);
             Assert.AreEqual(actual, diff);
+        }
+
+        [Test]
+        public void IsDefault_ShouldReturnTrue_WhenDefault()
+        {
+            Assert.IsTrue(new DateTimeCalculator().IsDefault(default));
+        }
+        
+        [Test]
+        public void IsDefault_ShouldReturnTrue_WhenNotDefault()
+        {
+            Assert.IsFalse(new DateTimeCalculator().IsDefault(DateTime.Now));
         }
     }
 }
--- UnderTest/DateTimeCalculator.cs
+++ UnderTest/DateTimeCalculator.cs
@@ -5,5 +5,13 @@ namespace UnderTest
     public class DateTimeCalculator
     {
         public TimeSpan Diff(DateTime a, DateTime b) => a - b;
+
+        public bool IsDefault(DateTime dateTime)
+        {
+            if (dateTime == default)
+                return true;
+            else
+                return false;
+        }
     }
 }
